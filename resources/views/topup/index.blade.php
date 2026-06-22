1f14ffd4cc1dc0d2428b096b86f804612bbc314a
Felix <felixlinandy@gmail.com>
2026-06-22 14:55:17 +0700

    Credit Template

---
+++ resources/views/topup/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="flex justify-center items-center">          
+        <h1 class="text-white font-bold text-3xl w-50 h-15 py-2 text-center 
+            border-2 bg-gray-800 border-mavs-navy rounded-full m-5 shadow-lg shadow-mavs-navy">
+            Top Up
+        </h1>
+    </div>
+
+@endsection
