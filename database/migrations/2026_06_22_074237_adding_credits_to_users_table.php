dc195811076c50980cf74ebdfacfcdaaaacd7ae6
NormTryToCode <wijayachandra160@gmail.com>
2026-06-22 14:50:30 +0700

    Fixing the credits on user migration

--- database/migrations/2026_06_22_074237_adding_credits_to_users_table.php
+++ database/migrations/2026_06_22_074237_adding_credits_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-           $table->unsignedBigInteger('credits')->default(100);
+           $table->decimal('credits', 10, 2)->default(100.75); 
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-           $table->dropColumn('credits');  
+           $table->dropColumn('credits'); 
         });
     }
 };
